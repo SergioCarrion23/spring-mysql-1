025da2481a0558b29c489f01effe7c1ecb824fce
sigif <sigifedin123@gmail.com>
2022-07-08 04:25:46 +0200

    Merge remote-tracking branch 'origin/main' into main

---
+++ src/main/java/ec/edu/insteclrg/domain/Provincia.java
@@ -0,0 +1,24 @@
+package ec.edu.insteclrg.domain;
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+@Entity 
+@Getter
+@Setter
+public class Provincia {
+@Id
+@Column(updatable = false,nullable = false)
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;	
+@Column(nullable = false)
+private String nombre;
+
+@Column
+private String acronimo;
+}
